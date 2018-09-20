ceeb0f54bdb2940069f787ac41f5e4a6b85dd9f1
Koy Saeteurn <kosatun@aol.com>
2018-09-20 06:23:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(end)
     resp = Rack::Response.new 
     now = Time.now
-    if (hour < 12)
+    if (now < 12)
       resp.write "Good Morning!"
     else 
       resp.write "Good Afternoon!"
